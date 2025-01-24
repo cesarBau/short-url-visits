193784a4e83229b8eced9b38a02be440c77ad015
Cesar Bautista <68753523+cesarBau@users.noreply.github.com>
2025-01-23 19:53:17 -0600

    Se agrega configuracion de amqp

--- src/main/java/com/rabbit/visits/config/AmqpConfig.java
+++ src/main/java/com/rabbit/visits/config/AmqpConfig.java
@@ -1,5 +1,36 @@
 package com.rabbit.visits.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:other.properties")
 public class AmqpConfig {
 
+    @Value("${amqp.queue}")
+    public String QUEUE_NAME;
+
+    @Value("${amqp.exchange}")
+    public String EXCHANGE_NAME;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, false);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("routing.key.#");
+    }
 }
